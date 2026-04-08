b689cb1bf896c603385d3cbfea90c0b24c88f461
TutonGhosh <404ghosh@gmail.com>
2026-04-09 00:19:15 +0600

    js added to call history

--- js/script.js
+++ js/script.js
@@ -46,3 +46,47 @@ function copyNumber(btn)
     copyCnt++;
     navCopy.innerText = copyCnt;
 }
+
+// Call Function
+function call(callCard)
+{
+    const call = callCard.closest('.card');
+
+    const navCoin = document.getElementById('nav-coin');
+    const coin = parseInt(navCoin.textContent);
+    let perCallCoin = coin - 20;
+    if(perCallCoin >= 20 || perCallCoin === 0)
+    {
+        navCoin.textContent = perCallCoin;
+    }
+    else
+    {
+        return;
+    }
+
+    const name = call.querySelector('#card-name').textContent;
+    const number = call.querySelector('#emergency-number').textContent;
+
+    const time = new Date().toLocaleTimeString();
+
+    const div = document.createElement('div');
+    div.innerHTML = 
+    `<div class="bg-gray-200 rounded-lg p-2 px-5 flex items-center justify-between">
+            <div class="space-y-1">
+                <h1 class="text-lg font-semibold">${name}</h1>
+                <p class="text-base text-[#5C5C5C] font-medium">${number}</p>
+            </div>
+            <div>
+                <p class="text-base font-medium">${time}</p>
+            </div>
+    </div>`
+
+    const callHistoryCards = document.getElementById('call-history-cards');
+    callHistoryCards.appendChild(div);
+}
+// Clear History Function
+function clearHistory()
+{
+    const callHistoryCards = document.getElementById('call-history-cards');
+    callHistoryCards.innerHTML = ``;
+}
